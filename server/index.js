60516d3836c51bceff0741398f716e3372538468
Jerry Wu <wuj828@gmail.com>
2018-09-26 16:44:20 -0700

    Updated schema.sql with alter table to handle cascading delete for shared foreign keys.

--- server/index.js
+++ server/index.js
@@ -4,10 +4,11 @@ var db = require ('../database/index.js');
 const path = require ('path');
 var cors = require ('cors');
 
-app.use (cors ());
-app.use (express.static (path.join (__dirname + '/../public')));
+app.use(cors ());
+app.use(express.static (path.join (__dirname + '/../public')));
 
-app.get (`/artist/:id/relatedArtists`, (req, res) => {
+app.get('/artist/:id/relatedArtists', (req, res) => {
+  console.log('Received GET with: ', req.params);
   db.getRelatedArtists (req.params.id, (error, data) => {
     if (error) {
       res.status (503).send (error);
@@ -17,6 +18,22 @@ app.get (`/artist/:id/relatedArtists`, (req, res) => {
   });
 });
 
-app.listen (3002, () => {
+app.post('/artist/:id/relatedArtists', (req, res) => {
+  console.log('Receiving post request.');
+});
+
+app.put('/artist/:id/relatedArtists', (req, res) => {
+  console.log('Receiving put request.');
+});
+
+app.delete('/artist/:id/relatedArtists', (req, res) => {
+  console.log('Receiving delete request.');
+});
+
+app.options('', (req, res) => {
+  console.log('Here are your options: get, post, put, delete.')
+});
+
+app.listen(3002, () => {
   console.log ('listening on port 3002!');
 });
